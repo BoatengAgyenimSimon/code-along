c09eb5d3008f8f2d48a5537e1f12ff62db2ed50d
BoatengAgyenimSimon <110113128+BoatengAgyenimSimon@users.noreply.github.com>
2022-08-14 13:21:24 -0700

    Add files via upload
    code along

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Button from './components/Button'
+import Header from './components/Header'
+import {Image} from './components/Image'
+import Navbar from './components/Navbar'
+import Practice from './components/Practice'
+
+const App = () => {
+  return (
+    <div>
+      {/* <Navbar/> */}
+      {/* <Header/>
+      <Button label="Add to Cart"/>
+      <Button label="Read More"/>
+      <Button label="Register"/>
+      <Button label="Login"/>
+      <Image/> */}
+      <Practice/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({label}) => {
+  return (
+    <button className="border-2 border-orange-500 py-2 px-5 rounded-full text-orange-500 hover:bg-orange-500 hover:text-white">{label}</button>
+  )
+}
+
+export default Button
